e69d3af36f69fb49ee9127b45ee7d389386282cc
Neeraj Yadav <neerajydv1113@gmail.com>
2025-05-18 23:06:13 +0530

    Add files via upload

---
+++ BooleanEx.java
@@ -0,0 +1,10 @@
+public class BooleanEx {
+    public static void main(String[] args) {
+        // boolean b1=True;//error
+        boolean b1=true;
+        System.out.println(b1);
+
+        boolean b2=false;
+        System.out.println(b2);
+    }
+}
