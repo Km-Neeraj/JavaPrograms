328a401dc722b4b510ea8764486019d85ff58a12
Neeraj Yadav <neerajydv1113@gmail.com>
2025-05-20 23:47:21 +0530

    Add files via upload

---
+++ GappingRule.java
@@ -0,0 +1,18 @@
+public class GappingRule{
+    public static void main(String[]args){
+        System 
+        . 
+        
+        out
+        
+        
+        .
+        
+        println
+        (
+            "Hii"
+            )
+            ;
+        System.out.println("Hello");
+    }
+}
