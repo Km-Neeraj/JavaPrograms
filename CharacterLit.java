175c3b2efe552e06da1d4ac42a3a51287caf6055
Neeraj Yadav <neerajydv1113@gmail.com>
2025-05-22 23:55:23 +0530

    Add files via upload

---
+++ CharacterLit.java
@@ -0,0 +1,16 @@
+public class CharacterLit {
+    public static void main(String[] args) {
+        char c1='h';
+        System.out.println(c1);
+
+        char c2=105;//Ascii value(0-255)
+        System.out.println(c2);
+
+        char c3='\u0068';//Unicode(0-65535)
+        System.out.println(c3);
+
+        //'a'->97 'b'->98 and so on
+        //'A'->65 'B'-66 AND SO ON
+        //'0'->48 '1'->49 and so on
+    }
+}
