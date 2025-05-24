f5286bc06d51daf07fae20c1dd211820999b7d93
Neeraj Yadav <neerajydv1113@gmail.com>
2025-05-24 23:29:12 +0530

    Add files via upload

---
+++ StringEx2.java
@@ -0,0 +1,16 @@
+
+public class StringEx2 {
+
+	public static void main(String[] args) {
+//		char c[]="Incapp";
+		
+		String s1=new String("Incapp");
+		System.out.println(s1);
+		System.out.println(s1.toUpperCase());
+		
+		String s2="Incapp";
+		System.out.println(s2);
+		System.out.println(s2.toUpperCase());
+	}
+
+}
