b8f09521c107e5e1373c1ce42cf543555c47f7c7
Neeraj Yadav <neerajydv1113@gmail.com>
2025-05-25 23:24:42 +0530

    Add files via upload

---
+++ StringLit.java
@@ -0,0 +1,12 @@
+public class StringLit {
+    public static void main(String[] args) {
+        String s="Hii";
+        System.out.println(s);
+
+        String s2="12";//String literal
+        System.out.println(s2);
+
+        String s3=null;
+        System.out.println(s3);
+    }
+}
