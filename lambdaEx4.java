734757ef22c2eab43e7ad1df05ad1b36a55dea15
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-09 23:01:56 +0530

    Add files via upload

---
+++ lambdaEx4.java
@@ -0,0 +1,19 @@
+
+interface abc4
+{
+                         int fun(int x);
+}
+public class lambdaEx4 {
+     public static void main(String[] abc)
+     {
+                         abc4 a=x->
+                          x*x;
+     
+
+     System.out.println("Result = "+a.fun(10));
+     }
+
+                         
+}
+
+//output= Result = 100
