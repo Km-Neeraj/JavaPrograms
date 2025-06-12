121ce39c71acf7dbdf6bcffc06883ce4a91e81bf
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-12 22:28:36 +0530

    Add files via upload

---
+++ Arr1.java
@@ -0,0 +1,20 @@
+public class Arr1 {
+   public static void main(String[] abc)
+   {
+
+      int [] a=new int[5];  
+        a[0]=11;
+        a[1]=12;
+        a[2]=23;
+        a[3]=90;
+        a[4]=87;
+        System.out.println("Array elements are");
+
+        for(int num: a)
+        {
+                         System.out.print(num + "  ");
+        }
+
+   }
+                         
+}
