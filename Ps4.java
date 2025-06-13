a2be2205037e7ccb0e93196abf318f943af47590
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-13 12:39:42 +0530

    Add files via upload

---
+++ Ps4.java
@@ -0,0 +1,15 @@
+
+
+class Test{
+                         static void show()
+                         {
+                             System.out.println("Static");
+                         }
+}
+public class Ps4 {
+                         public static void main(String[] args) {
+                             Test t=null;
+                             t.show();
+                         }
+                         
+}
