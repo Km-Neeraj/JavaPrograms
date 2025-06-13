ee07a90c00c03061afaaf1d9d6f83b337c0864d0
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-13 13:22:15 +0530

    Add files via upload

---
+++ Ps9.java
@@ -0,0 +1,16 @@
+class Test {
+void show(final int x) {
+// x = 10;
+System.out.println(x); 
+}
+}
+
+
+public class Ps9 {
+
+                         public static void main(String[] args) {
+                                                  Test t = new Test();
+t.show(5);
+                         }
+                         
+}
