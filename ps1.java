e55431ba824e61630078db496b201351061a9488
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-13 12:06:23 +0530

    Add files via upload

---
+++ ps1.java
@@ -0,0 +1,22 @@
+abstract class A
+{
+     void show()  
+     {
+                         System.out.println("A");
+     } 
+
+}
+
+class B extends A
+{
+
+}
+public class ps1 {
+                         public static void main(String[]abc)
+                         {
+                              B ob=new B();
+                              ob.show();
+                              
+                         }
+                         
+}
