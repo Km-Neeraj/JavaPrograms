7e921cf3fd9bf0706faef6c923b6d4c671129883
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-16 19:39:59 +0530

    Add files via upload

---
+++ RelartionalOprEx.java
@@ -0,0 +1,14 @@
+
+public class RelartionalOprEx {
+
+	public static void main(String[] args) {
+		int a=9,b=10;
+		
+		boolean c = ++a!=--b;
+		System.out.println(c);
+		
+		c= --a == b;
+		System.out.println(c);
+	}
+
+}
