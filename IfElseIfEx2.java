bb5bd0a705d4949cf185b656849e21969dd1592d
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-19 00:29:33 +0530

    Add files via upload

---
+++ IfElseIfEx2.java
@@ -0,0 +1,27 @@
+
+public class IfElseIfEx2 {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter any three number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		
+		if(a==b&&a==c) {
+			System.out.println("All are equal");
+		}
+		else if(a>b && a>c) {
+			System.out.println(a+" is greatest");
+		}
+		else if(b>c) {
+			System.out.println(b+" is greatest");
+		}
+		else {
+			System.out.println(c+" is greatest");
+		}
+		
+		sc.close();
+	}
+
+}
