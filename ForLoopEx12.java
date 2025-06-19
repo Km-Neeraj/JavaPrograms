49efff28b0edd0f024934dd0138744c1bdcee4c5
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-19 20:17:51 +0530

    Add files via upload

---
+++ ForLoopEx12.java
@@ -0,0 +1,10 @@
+
+public class ForLoopEx12 {
+	public static void main(String[] args) {
+		byte a=1;
+		System.out.println(a+127); 
+		System.out.println((byte)(a+127)); 
+		a=127;
+		System.out.println(++a); 
+	}
+}
