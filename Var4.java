45794359a0862d1ef4fe123c7fd45688b21df790
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-23 08:13:41 +0530

    Add files via upload

---
+++ Var4.java
@@ -0,0 +1,14 @@
+//Local Variable Type Inference- var
+//Introduced in JAVA 10
+
+public class Var4 {
+
+	public static void main(String[] args) {
+
+		int a[]=new int[] {2,8,5};
+		for(var x:a) {
+			System.out.println(x);
+		}
+	}
+
+}
