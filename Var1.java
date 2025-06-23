178f7a29d3e85d0d6ceb36c372a6026f92931b98
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-23 08:14:30 +0530

    Add files via upload

---
+++ Var1.java
@@ -0,0 +1,23 @@
+//Local Variable Type Inference- var
+//Introduced in JAVA 10
+public class Var1 {
+
+	public static void main(String[] args) {
+		var a=12.3;
+		System.out.println(a);
+		var b="Incapp";
+		System.out.println(b);
+//		var c;//error
+//		System.out.println(c);
+		
+//		int d;
+//		System.out.println(d);//error
+		
+//		var a; //error
+//		a=90;
+		
+		int d;
+		d=90;
+	}
+
+}
