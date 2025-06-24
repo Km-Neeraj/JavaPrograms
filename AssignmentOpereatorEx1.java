b320cb3a89bf0553dea8ee7e6b7b788a980c6a3f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-24 09:39:56 +0530

    Add files via upload

---
+++ AssignmentOpereatorEx1.java
@@ -0,0 +1,28 @@
+
+public class AssignmentOpereatorEx1 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		a=12;
+		System.out.println(a);
+		
+		a=14;
+		a=66;
+		System.out.println(a);
+		
+//		12=a;//error
+		
+		
+		int p=15,q,r=90;
+		
+		
+		int j,k;
+		j=12;
+		k=12;
+		
+//		or
+		j=k=12;
+	}
+
+}
