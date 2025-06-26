08c51d6b0141d75d9174348c9a134f17f3318461
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-26 21:52:11 +0530

    Add files via upload

---
+++ HashSetCollectionEx.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class HashSetCollectionEx {
+
+	public static void main(String[] args) {
+		
+		HashSet h=new HashSet();
+		h.add(12);
+		h.add("Ram");
+		h.add(12.40);
+		h.add('h');
+		h.add(true);
+	    h.add("12");
+	    
+	    System.out.println(h);
+	    
+	    
+	    h.remove(12);
+	    
+	    h.remove("12");
+	    
+	    System.out.println(h);
+	    
+	    for(Object o:h)
+	    {
+	    	System.out.println(o);
+	    }
+	    
+	   
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
