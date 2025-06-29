865c62e4ccb618854350156c95fca661b5c8068b
Neeraj Yadav <neerajydv1113@gmail.com>
2025-06-29 21:27:06 +0530

    Add files via upload

---
+++ PatternEx1.java
@@ -0,0 +1,14 @@
+
+public class PatternEx1 {
+	public static void main(String[] args) {
+		int row=5;
+		int star=5;
+		for(int j=1;j<=row;j++) {
+			for(int i=1;i<=star;i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
