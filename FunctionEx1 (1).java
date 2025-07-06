4d55c34616d07f02e36bb587376ac678bf9d39e5
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-06 08:52:43 +0530

    Add files via upload

---
+++ FunctionEx1 (1).java
@@ -0,0 +1,17 @@
+
+public class FunctionEx1 {
+	
+	public static void main(String[] args) {
+		add();
+		System.out.println("hii");
+		add();
+		add();
+	}
+	
+	static void add() {
+		int a=12;
+		int b=10;
+		int sum=a+b;
+		System.out.println("Sum :"+sum);
+	}
+}
