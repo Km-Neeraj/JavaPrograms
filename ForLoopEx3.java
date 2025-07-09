045abdf9e40acfeafae402fcc6e8f975fd2f4b34
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-09 15:24:40 +0530

    Add files via upload

---
+++ ForLoopEx3.java
@@ -0,0 +1,14 @@
+
+public class ForLoopEx3 {
+	public static void main(String[] args) {
+		for(int x=1;x++<=5;x++) {
+			System.out.println("Hii");
+		}
+		
+//		or
+		
+		for(int x=1;++x<=5;x++) {
+			System.out.println("Hii");
+		}
+	}
+}
