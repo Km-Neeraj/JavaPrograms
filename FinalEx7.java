88cdfd05e853168ef04a63dd95620e55e8050d1e
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-13 20:48:04 +0530

    Add files via upload

---
+++ FinalEx7.java
@@ -0,0 +1,19 @@
+public class FinalEx7
+{
+                         final int x;
+                         public FinalEx7()
+                         {
+                                                  x=10;
+                         }
+                         public static void main(String[]abc)
+                         {
+                            FinalEx7 ex=new FinalEx7();
+                            //ex.x=20;    //cannot assign a value to final variable x
+                            
+                            System.out.println(ex.x);
+                         }
+}
+
+
+                         
+
