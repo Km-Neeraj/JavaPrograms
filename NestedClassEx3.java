66d5fb056e13d4d5e0698a765e5ddd0175a129cb
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-13 20:49:23 +0530

    Add files via upload

---
+++ NestedClassEx3.java
@@ -0,0 +1,24 @@
+abstract class A
+{
+                         {
+                         System.out.println(1);
+                         }
+
+static 
+{
+                         System.out.println(2);
+}
+}
+public class NestedClassEx3 {
+                         public static void main(String[]abc)
+                         {
+                                                  A a=new A()
+                                                  {
+
+                                                  };
+                         }
+                         
+}
+
+/* 2
+   1 */
