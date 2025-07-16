2449e2c8addb6af3c992bf308f7b3f3c8e902bbc
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-16 15:45:05 +0530

    Add files via upload

---
+++ String6.java
@@ -0,0 +1,14 @@
+
+public class String6 {
+                         public static void main(String[]abc)
+                         {
+                            String c="Hello I Love Java";
+                            int start =2;
+                            int end=9;
+                            char s[]=new char[end-start];
+                            c.getChars(start,end,s,0);
+                            System.out.println(s);
+                         }
+                         
+}
+//llo I L
