3a36f88117594adee25b1cadf3766b72805dc2df
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-16 15:44:42 +0530

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,17 @@
+public class String5 {
+                         Integer i;
+                         int x;
+                         public String11(int y)
+                         {
+                                                  x=i+y;
+                                                  System.out.println(x);
+                         }
+                         public static void main(String[] abc)
+                         {
+                                                  new String5(new Integer(4));
+                         }
+
+                         
+}  
+
+//compilation fails
