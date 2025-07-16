c169e2847f94b7317bb311c50df47c2746d46f41
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-16 15:34:55 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,12 @@
+public class String1
+{
+                         public static void main(String[] args)
+                         {
+                                                  String obj="I"+"like"+"java";
+                                                  System.out.println(obj);
+                         }
+}
+//Ilikejava
+ 
+                         
+
---
+++ String2.java
@@ -0,0 +1,13 @@
+
+public class String2 {
+                         public static void main(String[]anb)
+                         {
+                                                  String obj="hello";
+                                                  String obj1="word";
+                                                  String obj2=obj;
+                                                  obj2=" world";
+                                                  System.out.println(obj+" "+obj2);
+                         }
+                         
+}
+//hello  world
