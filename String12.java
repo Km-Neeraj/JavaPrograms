c344dddf9f272d8cceee12b7e5291cad81ba13a5
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-19 23:51:27 +0530

    Add files via upload

---
+++ String12.java
@@ -0,0 +1,10 @@
+public class String12 {
+                         public static void main(String[]abc)
+                         {
+                                                  String a="hello i love java";
+                                                  System.out.println(a.indexOf('i')+" "+a.indexOf('o')+" "+a.lastIndexOf('i')+" "+a.lastIndexOf('o'));
+                         }
+                         
+}
+
+//6 4 6 9
