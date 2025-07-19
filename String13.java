f4d13cf55759a297942bf94493bb2690fef8897d
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-19 23:51:55 +0530

    Add files via upload

---
+++ String13.java
@@ -0,0 +1,10 @@
+public class String13 {
+                         public static void main(String[]abc)
+                         {
+                                                  char ch;
+                                                  ch="hello".charAt(1);
+                                                  System.out.println(ch);
+                         }
+                         
+}
+//e
