faeb33b4dda90244b818201a1816887aa07f0bf1
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-19 23:49:56 +0530

    Add files via upload

---
+++ String8.java
@@ -0,0 +1,12 @@
+
+public class String8 {
+                         public static void main(String[] abc)
+                         {
+                                                  char chars[]={'a','b','c','d','e','f'};
+                                                  String s1=new String(chars);
+                                                  String s2=new String(chars,3,2);
+                                                  System.out.println(s1+" "+s2);
+                         }
+                         
+}
+//abcdef de
