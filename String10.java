c702da1bf8c7b19de954ceeca46c110128b82c4a
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-19 23:50:50 +0530

    Add files via upload

---
+++ String10.java
@@ -0,0 +1,15 @@
+
+public class String10 {
+
+                         public static void main(String[]abc)
+                         {
+                            String c="Hello i love java";
+                            int start=2;
+                            int end=9;
+                            char s[]=new char[end-start];
+                            c.getChars(start,end,s,0);
+                            System.out.println(s);
+                         }
+                         
+}
+//llo i l
