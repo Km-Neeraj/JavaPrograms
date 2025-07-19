04db6128b5fb42395f752caba0c7bdf2221d07af
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-19 23:50:23 +0530

    Add files via upload

---
+++ String9.java
@@ -0,0 +1,21 @@
+public class String9 {
+                         Integer i;
+                         int x;
+                         public Boxer1(int y)
+                         {
+                            x=i+y;
+                            System.out.println(x);
+                         }
+
+                         public static void main(String[]abc)
+                         {
+                             new String9(new Integer(4));
+                         }
+                         
+}
+
+/*    error: invalid method declaration; return type required
+                         public Boxer1(int y)
+                                ^
+
+           error: compilation failed */
