150d8687f9201938694f941575d8a4bc2272724c
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-20 13:23:20 +0530

    Add files via upload

---
+++ String14.java
@@ -0,0 +1,10 @@
+public class String14 {
+                         public static void main(String[]abc)
+                         {
+                           String s1="Hello World";
+                           String s2=s1.substring(0,4);
+                           System.out.println(s2);
+                         }
+                         
+}
+//Hell
