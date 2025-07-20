152113212e9a3ebb452cc8504ef4db4898b2322b
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-20 13:23:52 +0530

    Add files via upload

---
+++ String15.java
@@ -0,0 +1,11 @@
+public class String15 {
+                         public static void main(String[]abc)
+                         {
+                            String s="Hello World";
+                            int i=s.indexOf('o');
+                            int j=s.lastIndexOf('I');
+                            System.out.print(i+" "+j);
+                         }
+                         
+}
+//4 -1
