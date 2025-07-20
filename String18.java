7c06ac0058f02b0243b4e09fe63ccb17e3a0a64f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-20 13:25:38 +0530

    Add files via upload

---
+++ String18.java
@@ -0,0 +1,10 @@
+public class String18 {
+
+                         public static void main(String[]abc)
+                         {
+                                                  String a="hello i love java";
+                         System.out.println(a.indexOf('e')+" "+a.indexOf('a')+" "+a.lastIndexOf('I')+" "+a.lastIndexOf('v'));
+                         }
+                         
+}
+//1 14 -1 15
