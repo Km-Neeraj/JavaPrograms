efb30d2aac9ab17a22355282bcadf66ad5608ebb
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-20 13:24:26 +0530

    Add files via upload

---
+++ String16.java
@@ -0,0 +1,10 @@
+public class String16 {
+                         public static void main(String[]abc)
+                         {
+                                                  StringBuffer s1=new StringBuffer("Hello World");
+                                                  s1.insert(6,"Good");
+                                                  System.out.println(s1);
+                         }
+                         
+}
+//Hello GoodWorld
