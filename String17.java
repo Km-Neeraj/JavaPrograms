c320bc76150d3af5c0236fc4c598d0bb5a4aaaa8
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-20 13:24:57 +0530

    Add files via upload

---
+++ String17.java
@@ -0,0 +1,11 @@
+public class String17 {
+
+                         public static void main(String[]abc)
+                         {
+                                                  StringBuffer s1=new StringBuffer("Hello");
+                                                  StringBuffer s2=s1.reverse();
+                                                  System.out.println(s1+" "+s2);
+                         }
+                         
+}
+//olleH olleH
