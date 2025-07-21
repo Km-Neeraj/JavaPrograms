a2181c312219519bb4bfa923c2ea1449280fcdd0
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-21 12:30:05 +0530

    Add files via upload

---
+++ Array1Ex1.java
@@ -0,0 +1,13 @@
+public class Array1Ex1 {
+                         public static void main(String[]abc)
+                         {
+                                                  int array_variable[]=new int[10];
+                                                  for(int i=0;i<10;++i)
+                                                  {
+                                                    array_variable[i]=i;
+                                                    System.out.print(array_variable[i]+" ");
+                                                  }
+                         }
+                         
+}
+//0 1 2 3 4 5 6 7 8 9 
