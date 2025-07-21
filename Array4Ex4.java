0028f25d029280207dc2b51dbd7e8676dfa526b7
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-21 12:33:37 +0530

    Add files via upload

---
+++ Array4Ex4.java
@@ -0,0 +1,22 @@
+public class Array4Ex4 {
+                         public static void main(String[]abc)
+                         {
+                                                  char array_variable[]=new char[10];
+                                                  for(int i=0;i<10;++i)
+                                                  {
+                                                                           array_variable[i]='i';
+                                                                           System.out.println(array_variable[i]+" ");
+                                                  }
+                         }
+                         
+}
+/* i 
+i 
+i 
+i
+i
+i
+i
+i
+i
+i */
