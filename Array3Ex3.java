f39aa30163b94450f4aa6f1a4a6f61174aa58340
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-21 12:31:43 +0530

    Add files via upload

---
+++ Array3Ex3.java
@@ -0,0 +1,12 @@
+public class Array3Ex3 {
+
+                         public static void main(String[]abc)
+                         {
+                                                  int arr[]=new int[]{0,1,2,3,4,5,6,7,8,9};
+                                                  int n=6;
+                                                  n=arr[arr[n]/2];
+                                                  System.out.println(arr[n]/2);
+                         }
+                         
+}
+//1
