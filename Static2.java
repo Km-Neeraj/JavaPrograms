5b81f16bf53e34fae1e8641b820640fe9e995ce3
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-22 14:37:10 +0530

    Add files via upload

---
+++ Static2.java
@@ -0,0 +1,13 @@
+public class Static2 {
+                        static int x=5;
+                        public static void main(String[]args)
+                        {
+                         int x=10;
+                         System.out.println("x = "+x);
+                         System.out.println("x = "+Static2.x);
+                        }
+
+                         
+}
+//x = 10
+//x = 5
