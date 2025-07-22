e3374ac27a8c923e9164d8a6ffd3f6a1e696c3df
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-22 14:36:46 +0530

    Add files via upload

---
+++ Static1.java
@@ -0,0 +1,12 @@
+public class Static1 {
+                         static int x=10;
+                         static {
+                                                  x+=5;
+                         }
+                         public static void main(String[]abc)
+                         {
+                             System.out.println("x= "+x);
+                         }
+                         
+}
+//x= 15
