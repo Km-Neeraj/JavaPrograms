abe914390306eb4306dd59e1eb22951d327a9f93
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-22 14:38:03 +0530

    Add files via upload

---
+++ Static4.java
@@ -0,0 +1,22 @@
+
+class A
+{
+    static int x;
+    static{
+                         System.out.println("hii");
+    }
+    static 
+    {
+                         System.out.println("Hello");
+    }
+}
+public class Static4 {
+                         public static void main(String[]abc)
+                         {
+                                                  System.out.println(A.x);
+                         }
+                         
+}
+/* hii
+   Hello
+   0  */
