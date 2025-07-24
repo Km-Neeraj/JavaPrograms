6d4d45bcd783899d314a7de5b8ff1528ef174561
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-24 10:38:51 +0530

    Add files via upload

---
+++ ExceptionEx7.java
@@ -0,0 +1,13 @@
+public class ExceptionEx7 {
+                         public static void main(String[]abc)
+                         {
+                                                  try{
+                                                       System.out.print("Hello");
+                                                  }
+                                                  finally{
+                                                                           System.out.println("Finally");
+                                                  }
+                         }
+                         
+}
+//HelloFinally
