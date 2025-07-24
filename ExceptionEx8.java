be204ea24fe3e2cb7560fc6ccab105331c77311c
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-24 10:39:11 +0530

    Add files via upload

---
+++ ExceptionEx8.java
@@ -0,0 +1,13 @@
+public class ExceptionEx8 {
+                         public static void main(String[]abc)
+                         {
+                                                  try{
+                                                        return;
+                                                  }
+                                                  finally{
+                                                                           System.out.println("Finally");
+                                                  }
+                         }
+                         
+}
+//Finally
