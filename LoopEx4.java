87e2a59a72187284941436ea32edb6ac1831d224
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-26 21:38:44 +0530

    Add files via upload

---
+++ LoopEx4.java
@@ -0,0 +1,16 @@
+public class LoopEx4 {
+                         public static void main(String[]abc)
+                         {
+                            int i=5;
+                            while(i>0)
+                            {
+                              System.out.print(i--+" ");
+                              if(i==3)
+                              {
+                                break;
+                              }
+                            }
+                         }
+                         
+}
+//5 4 
