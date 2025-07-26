cdc1c0c6d5aed2502c40916dc08ffeccce507f32
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-26 21:37:29 +0530

    Add files via upload

---
+++ LoopEx2.java
@@ -0,0 +1,13 @@
+public class LoopEx2 {
+                         public static void main(String[]abc)
+                         {
+                          int i=1;
+                          while(i<=5)
+                          {
+                            System.out.print(i+" ");
+                            i+=2;
+                          }
+                         }
+                         
+}
+//1 3 5 
