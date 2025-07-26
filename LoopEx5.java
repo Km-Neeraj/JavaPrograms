b450214dde46485df46ff981e771351ba0d05d98
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-26 21:39:05 +0530

    Add files via upload

---
+++ LoopEx5.java
@@ -0,0 +1,17 @@
+public class LoopEx5 {
+                         public static void main(String[]abc)
+                         {
+                            for(int i=1;i<=3;i++)
+                            {
+                                                  for(int j=3;j>=i;j--)
+                                                  {
+                                                     System.out.print(j+" ");
+                                                  }
+                                                  System.out.println();
+                            }
+                         }
+                         
+}
+/*  3 2 1 
+    3 2
+    3     */
