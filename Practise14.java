bc743f088ab725b0c6939c92bc414e27cb3fc06f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-29 20:43:51 +0530

    Add files via upload

---
+++ Practise14.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Practise14 {
+                         public static void main(String[]abc)
+                         {
+                           int row;
+                           Scanner sc=new Scanner(System.in);
+                           System.out.println("Enter the number of row");
+                           row=sc.nextInt();
+                           int half=row/2;
+                           for(int i=1;i<=row;i++)
+                           {
+                              for(int j=1;j<=2*row;j++)
+                              {
+                                   if(j==(2*half)||i==half)
+                                   {
+                                                  System.out.print("*");
+                                   }
+                                   else
+                                   {
+                                      System.out.print(" ");
+                                   }
+                              }
+                              System.out.println();
+                           }
+
+                         }
+                         
+}
+/*    
+Enter the number of row
+10
+         *
+         *
+         *
+         *
+********************
+         *
+         *
+         *
+         *
+         *       */
