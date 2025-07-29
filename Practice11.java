601bd02ec03de579acec118d7d5129448950c333
Neeraj Yadav <neerajydv1113@gmail.com>
2025-07-29 20:42:18 +0530

    Add files via upload

---
+++ Practice11.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Practice11 {
+                         public static void main(String[]abc)
+                         {
+                            int row;
+                            Scanner sc=new Scanner(System.in);
+                            System.out.println("Enter the number of row");
+                            row=sc.nextInt();
+                            for(int i=1;i<=row;i++)
+                            {
+                               for(int j=1;j<=i;j++)
+                               {
+                                 if(j==1||j==i)
+                                 {
+                                   System.out.print("*");
+                                 }
+                                 else{
+                                         System.out.print(" ");
+                                 }
+                               }
+                               System.out.println();
+                            }
+
+                            for(int i=row-1;i>=1;i--)
+                            {
+                               for(int j=1;j<=i;j++)
+                               {
+                                 if(j==1||j==i)
+                                 {
+                                   System.out.print("*");
+                                 }
+                                 else{
+                                         System.out.print(" ");
+                                 }
+                               }
+                               System.out.println();
+                            }
+
+
+                         }
+                         
+}
