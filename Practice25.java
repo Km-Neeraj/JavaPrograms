af86a21621371ff5a22ab92767e7c5093a5fc60f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-02 08:13:21 +0530

    Add files via upload

---
+++ Practice25.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Practice25 {
+                         public static void main(String[]abc)
+                         {
+                              int row;
+                              Scanner sc=new Scanner(System.in);
+                              System.out.println("Enter the number of row");
+                              row=sc.nextInt();
+                              for(int i=1;i<=row;i++)
+                              {
+                                for(int j=1;j<=i;j++)
+                                {
+                                    System.out.print(j+" ");
+                                }
+                                System.out.println();
+                              }
+                         }
+                         
+}
+/*  
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5    */
