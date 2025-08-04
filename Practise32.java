043d609705a6ba0276345d727115adfe22dd7c17
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-04 06:48:23 +0530

    Add files via upload

---
+++ Practise32.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Practise32 {
+                         public static void main(String[]abc)
+                         {
+                             //Creating a HashSet
+                             HashSet<Integer> s=new HashSet<>();
+                             //Add elements into HashSet
+                              s.add(8);
+                              s.add(9);
+                              s.add(7);
+                               s.add(5);
+                               s.add(7);
+                               s.add(5);
+                               //print HashSet elements 
+                               System.out.println("HashSet elements are");
+                               System.out.println(s);
+                         }
+                         
+}
