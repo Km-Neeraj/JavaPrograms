696d20bfbc95909a40766c0312a6b1a59387d61c
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-04 06:51:07 +0530

    Add files via upload

---
+++ Practise34.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Practise34 {
+
+                         public static void main(String[]abc)
+                         {
+                            //Create HashSet
+                         HashSet s=new HashSet();
+                         //Add elements in HashSet
+                         s.add("Ram");
+                         s.add(12);
+                         s.add(89);
+                         s.add(45.21);
+                         s.add(78.8f);
+                         s.add("Ram");
+                          System.out.println("Collection Elements");
+                         for(Object ob:s)
+                         {
+                               System.out.println(ob);                   
+                         } 
+                         
+                         }      
+                         
+}
