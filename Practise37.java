ddc3be8033e6adddaf231fb66d3b395d993862aa
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-05 18:10:41 +0530

    Add files via upload

---
+++ Practise37.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Practise37
+{
+    public static void main(String[]abc)
+    {
+       List<String> list=Arrays.asList("Zara","Ram","Shyam","Yash");
+       Collections.sort(list);
+
+       System.out.println("Sorted String ="+list);
+    }
+}
