f26aecbad1132dba9995900e9bd3deb05cd6d3cd
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-05 18:11:13 +0530

    Add files via upload

---
+++ Practise38.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Practise38 {
+                         public static void main(String[]abc)
+                         {
+                            List<Integer> li=Arrays.asList(1,2,33,45,12,67,90,90);
+                            Collections.sort(li);
+
+                            System.out.println("Arrange elements in ascending order ="+li);
+                         }
+                         
+}
