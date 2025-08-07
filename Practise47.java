40f0a0babd865d86d7783093b75a15052bd63ac2
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-07 21:04:31 +0530

    Add files via upload

---
+++ Practise47.java
@@ -0,0 +1,27 @@
+public class Practise47 {
+                         public static void main(String[]abc)
+                         {
+                                                  int[] arr=new int[10];
+                                                  arr[0]=100;
+                                                  arr[1]=90;
+                                                  arr[2]=11;
+                                                  arr[3]=6;
+                                                  arr[4]=23;
+                                                  arr[5]=43;
+                                                  arr[6]=11;
+                                                  arr[7]=100;
+                                                  arr[8]=17;
+                                                  arr[9]=14;
+                                                  
+
+                                                  System.out.println("Array Elements are");
+                                                  for(int i=0;i<arr.length;i++)
+                                                  {
+                                                     System.out.print(arr[i]+" ");
+                                                  }
+                         }
+                         
+}
+
+//Array Elements are
+//100 90 11 6 23 43 11 100 17 14 
