f922c30b3db164fbc19e168ec720d5ae443f7371
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-09 23:03:20 +0530

    Add files via upload

---
+++ Practise55.java
@@ -0,0 +1,10 @@
+public class Practise55 {
+                         public static void main(String[]abc)
+                         {
+                             int x=10;
+                             int y=3;
+                             System.out.println("x/y="+(x/y));
+                         }
+                         
+}
+//x/y=3
