4a96e2eea77ac8c556873911f6946711c036bac2
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-09 23:04:30 +0530

    Add files via upload

---
+++ Practise57.java
@@ -0,0 +1,9 @@
+public class Practise57 {
+                         public static void main(String[]abc)
+                         {
+                                                  int r=8-9*2/5+6;
+                                                  System.out.println(r);
+                         }
+                         
+}
+//11
