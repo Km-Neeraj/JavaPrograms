f3c0b603ad1f931d986f4dbc7586d804eb47e333
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-09 23:02:51 +0530

    Add files via upload

---
+++ Practise54.java
@@ -0,0 +1,11 @@
+public class Practise54 {
+
+public  static void main(String[]abc)
+{
+    int  i=5;
+    int j=3;
+    System.out.println("i+j="+(i+j));
+}
+                         
+}
+/*    i+j=8 */
