07f9fafd72ebdf533f93eef20fcde0a7eec8b043
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-10 11:07:32 +0530

    Add files via upload

---
+++ Practise59.java
@@ -0,0 +1,10 @@
+public class Practise59 {
+                         public static void main(String[]abc)
+                         {
+                            int a=10;
+                            int r=a*7-6/2+a++;
+                            System.out.println("Value of r="+r);
+                         }
+                         
+}
+//Value of r=77
