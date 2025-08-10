cffd318623a254163bc3c9b4a1999c6bf78bd9cf
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-10 11:08:36 +0530

    Add files via upload

---
+++ Practise61.java
@@ -0,0 +1,11 @@
+public class Practise61 {
+                         public static void main(String[]abc)
+                         {
+                              int a=10,b=20;
+                              int r=a++-b++*a;
+                              System.out.println("Value of result = "+r);
+
+                         }
+                         
+}
+//Value of result = -210
