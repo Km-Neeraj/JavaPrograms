e3f8c95aba4a7e7af33bcabb6cb3043a773f1443
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-10 11:07:04 +0530

    Add files via upload

---
+++ Practise58.java
@@ -0,0 +1,12 @@
+public class Practise58 {
+                         public static void main(String[]abc)
+                         {
+                             int a=10;
+                             int r=a+++3;
+                             System.out.println("value of r="+r);
+                             System.out.println("Value of a="+a);
+                         }
+                         
+}
+//value of r=13
+//Value of a=11
