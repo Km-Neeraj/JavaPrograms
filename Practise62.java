a5795a6bc5bbcc7e2e64b8bbc8dd8c539c461e0b
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-10 11:09:07 +0530

    Add files via upload

---
+++ Practise62.java
@@ -0,0 +1,15 @@
+public class Practise62 {
+                         public static void main(String[]abc)
+                         {
+                             int a=10,b=20;
+                             int r=++a<b?a--+b:b++/a++;
+                             System.out.println("value of res = "+r);
+                             System.out.println("Value of a = "+a);
+                             System.out.println("value of b = "+b);
+                         }
+                         
+}
+/*    
+value of res = 31
+Value of a = 10
+value of b = 20 */
