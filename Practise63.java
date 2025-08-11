474af24a43fabeaa148f3e0d6fb359d6113d6268
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-11 22:30:30 +0530

    Add files via upload

---
+++ Practise63.java
@@ -0,0 +1,11 @@
+public class Practise63 {
+                         public static void main(String[]abc)
+                         {
+                             var a=15;
+                             var b="Ram";
+                             var c=Double.parseDouble("12");
+                             System.out.println(a+b+c);
+                         }
+                         
+}
+//15Ram12.0
