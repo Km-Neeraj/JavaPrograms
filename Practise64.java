e3b4e2c2fb548be4c3e80edfda0e6e610144b970
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-11 22:32:03 +0530

    Add files via upload

---
+++ Practise64.java
@@ -0,0 +1,10 @@
+public class Practise64 {
+                         public static void main(String[]main)
+                         {
+                            int a=15;
+                            String b="Ram";
+                            double c=Double.parseDouble("12");
+                            System.out.println(a+b+c);
+                         }
+                         
+}
