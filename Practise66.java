ee15c52e6a78936fbd2c0cdc18ba4b5023ab1cab
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-11 22:31:26 +0530

    Add files via upload

---
+++ Practise66.java
@@ -0,0 +1,10 @@
+public class Practise66 {
+                         public static void main(String[]abc)
+                         {
+                            String str="true false true";
+                            boolean x=Boolean.valueOf(str);
+                            System.out.print(x);
+
+                         }
+                         
+}
