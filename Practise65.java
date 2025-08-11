8ac15068e115f0cb1d85be53baf0b99827794bb2
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-11 22:31:02 +0530

    Add files via upload

---
+++ Practise65.java
@@ -0,0 +1,11 @@
+
+                         
+public class Practise65
+{
+                         public static void main(String[]abc)
+                         {
+                            char a=(char)98;
+                            a=Character.toUpperCase(a);
+                            System.out.print(a);
+                         }
+}
