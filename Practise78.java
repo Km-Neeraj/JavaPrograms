3a9c656d3e0256f8e696f8ed3422276bbfd59b92
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-14 12:11:53 +0530

    Add files via upload

---
+++ Practise78.java
@@ -0,0 +1,15 @@
+public class Practise78 {
+                         Integer i;
+                         int x;
+                         public Practise78(int y)
+                         {
+                                                  x=i+y;
+                                                  System.out.println(x);
+                         }
+                         public static void main(String[]abc)
+                         {
+                         
+                         new Practise78(new Integer(4));
+                         }
+                         
+}
