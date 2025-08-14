68fa2c2fca5acb118a38497597e9c08fae84ad92
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-14 12:11:30 +0530

    Add files via upload

---
+++ Practise77.java
@@ -0,0 +1,10 @@
+public class Practise77 {
+                         public static void main(String[]abc)
+                         {
+                             char chars[]={'a','b','c','d','f'};
+                             String h1=new String(chars);
+                             String h2=new String(chars,3,2);
+                             System.out.println(h1+" "+h2);
+                         }
+                         
+}
