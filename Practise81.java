953015d8eefdc4a61ca899f2c0a58f32a326e31d
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-14 12:13:17 +0530

    Add files via upload

---
+++ Practise81.java
@@ -0,0 +1,9 @@
+public class Practise81 {
+                         public static void main(String[]abc)
+                         {
+                                                  char ch;
+                                                  ch="hello".charAt(1);
+                                                  System.out.println(ch);
+                         }
+                         
+}
