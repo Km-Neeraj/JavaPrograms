0881d9fbdac589a624567fa75255baa29fd87c79
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-14 12:12:17 +0530

    Add files via upload

---
+++ Practise79.java
@@ -0,0 +1,12 @@
+public class Practise79 {
+                         public static void main(String[]abc)
+                         {
+                            String c="Hello i love java";
+                            int start=2;
+                            int end=9;
+                            char s[]=new char[end-start];
+                            c.getChars(start,end,s,0);
+                            System.out.println(s);
+                         }
+                         
+}
