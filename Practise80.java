5412fa2764cb303761e652006a9ad558b75c5a28
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-14 12:12:41 +0530

    Add files via upload

---
+++ Practise80.java
@@ -0,0 +1,8 @@
+public class Practise80 {
+                         public static void main(String[]abc)
+                         {
+                                                  String a="hello i love java";
+                                                  System.out.println(a.indexOf('i')+" "+a.indexOf('o')+" "+a.lastIndexOf('i')+" "+a.lastIndexOf('o'));
+                         }
+                         
+}
