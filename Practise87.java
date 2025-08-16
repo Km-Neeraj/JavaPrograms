c4fda77d54612d19d0977f84657761164c0fe807
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-16 22:07:16 +0530

    Add files via upload

---
+++ Practise87.java
@@ -0,0 +1,69 @@
+// Insert At Last Position
+import java.util.*;
+class Node{
+             int data;
+             Node next;
+             Node head=null;
+
+             void createList(int n)
+             {
+                  Node tail=null;
+                  Scanner sc=new Scanner(System.in);
+                  for(int i=0;i<n;i++)
+                  {
+                     System.out.print("Enter "+(i+1)+" Node: ");
+                     Node newNode=new Node();
+                     int val=sc.nextInt();
+                     newNode.data=val;
+                     if(head==null)
+                     {
+                         head=newNode;
+                         tail=newNode;
+                     }
+                     else{
+                             tail.next=newNode;
+                             tail=newNode;
+                     }
+                  }
+             }
+             void printList()
+             {
+                    Node temp=head;
+                    while(temp!=null)
+                    {
+                        System.out.print(temp.data+" ");
+                        temp=temp.next;
+                    }
+             }
+             void insertAtLast(int val)
+             {
+                   Node newNode=new Node();
+                   newNode.data=val;
+                   Node temp=head;
+                   while(temp.next!=null)
+                   {
+                      temp=temp.next;
+                   }
+                   temp.next=newNode;
+                   temp=newNode;
+             }
+}
+public class Practise87 {
+                         public static void main(String[]abc)
+                         {
+                             int n;
+                             Scanner sc=new Scanner(System.in);
+                             System.out.println("Enter the number of nodes");
+                             n=sc.nextInt();
+                             Node list=new Node();
+                             list.createList(n);
+                             list.printList();
+                             System.out.println();
+                         System.out.println("Enter new node to insert at Last");
+                         int val=sc.nextInt();
+                         list.insertAtLast(val);
+                         list.printList();
+                         sc.close();
+                         }
+                         
+}
