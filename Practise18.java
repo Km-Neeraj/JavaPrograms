c69a6574d1d63beb39ada7b6c12d13e5cb364071
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-25 23:54:29 +0530

    Add files via upload

---
+++ Practise18.java
@@ -0,0 +1,9 @@
+public class Practise18 {
+                         public static void main(String[]abc)
+                         {
+                         int a=10,b=20;
+                         int s=a++ +b-- + --a + ++b;
+                         System.out.println(s);
+                         }
+                         
+}
