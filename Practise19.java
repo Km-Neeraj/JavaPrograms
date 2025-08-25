9a906b0ba20c6350ebaa27613897cc998dfe03fb
Neeraj Yadav <neerajydv1113@gmail.com>
2025-08-26 00:00:05 +0530

    Add files via upload

---
+++ Practise19.java
@@ -0,0 +1,18 @@
+public class Practise19 {
+                         public static void main(String[]abc)
+                         {
+                             for(int i=1;i<=5;i++)
+                             {
+                                for(int j=i;j<5;j++)
+                                {
+                                    System.out.print(" ");
+                                }
+                                for(int k=1;k<=i;k++)
+                                {
+                                   System.out.print("*"+" ");
+                                }
+                                System.out.println();
+                             }
+                         }
+                         
+}
